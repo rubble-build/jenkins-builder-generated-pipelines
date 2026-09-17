e1d2fe92351e71e3bc49b9c42f5d91b9a74f8a8d
Chai Feng <chaifeng@chaifeng.com>
2026-09-17 04:30:10 +0000

    Run Rubble workflow for v3quhux6x2wxcs5velotvdwtzffbqk5xm4elmmidy6uuq7h65xwa-rubble

---
+++ rubble-generated/v3quhux-rubble/lifecycle.sh
@@ -0,0 +1,90 @@
+export RUBBLE_CI_BUILDER='jenkins'
+# Generated lifecycle context. Source this file to use managed tools and runner commands.
+export rubble="${RUBBLE_EXECUTABLE:?RUBBLE_EXECUTABLE is required}"
+tool_information="$("${rubble}" -q info --format bash --tools)" || return
+eval "${tool_information}" || return
+unset tool_information
+
+lifecycle_dir="$(builtin cd -- "$(rubble-exec dirname -- "${BASH_SOURCE[0]}")" && builtin pwd)"
+export RUBBLE_CI_RUNTIME="${lifecycle_dir}/$(rubble-exec basename -- "${BASH_SOURCE[0]}")"
+repository_relative='../..'
+export RUBBLE_REPOSITORY="$(builtin cd -- "${lifecycle_dir}/${repository_relative}" && builtin pwd)"
+root_brick_relative='store/v3/qu/brk-v3quhux6x2wxcs5velotvdwtzffbqk5xm4elmmidy6uuq7h65xwa-rubble.brick'
+export RUBBLE_ROOT_BRICK="${lifecycle_dir}/${root_brick_relative}"
+export RUBBLE_ROOT_ID='v3quhux6x2wxcs5velotvdwtzffbqk5xm4elmmidy6uuq7h65xwa-rubble'
+export RUBBLE_ROOT_NAME='rubble'
+export RUBBLE_BRICK_FILE="${RUBBLE_BRICK_FILE:-${RUBBLE_ROOT_BRICK}}"
+export RUBBLE_BRICK_ID="${RUBBLE_BRICK_ID:-${RUBBLE_ROOT_ID}}"
+root_platform='x86_64-linux'
+export RUBBLE_BUILD_PLATFORM="${RUBBLE_BUILD_PLATFORM:-${root_platform}}"
+export RUBBLE_REMOTE_STORE='default'
+inventory_relative='release-inventory.txt'
+export RUBBLE_RELEASE_INVENTORY="${lifecycle_dir}/${inventory_relative}"
+publisher_relative='jenkins-publish.sh'
+export RUBBLE_PUBLISH_SCRIPT="${lifecycle_dir}/${publisher_relative}"
+
+declare -A rubble_hooks=()
+
+hook_relative='../../.rubble/hooks/pre-publish.sh'
+rubble_hooks['pre-publish']="${lifecycle_dir}/${hook_relative}"
+
+hook_relative='../../.rubble/hooks/post-publish.sh'
+rubble_hooks['post-publish']="${lifecycle_dir}/${hook_relative}"
+
+hook_relative='../../.rubble/hooks/pre-job.sh'
+rubble_hooks['pre-job']="${lifecycle_dir}/${hook_relative}"
+
+hook_relative='../../.rubble/hooks/post-job.sh'
+rubble_hooks['post-job']="${lifecycle_dir}/${hook_relative}"
+
+hook_relative='../../.rubble/hooks/prepare-release.sh'
+rubble_hooks['prepare-release']="${lifecycle_dir}/${hook_relative}"
+
+hook_relative='../../.rubble/hooks/post-release.sh'
+rubble_hooks['post-release']="${lifecycle_dir}/${hook_relative}"
+
+export RUBBLE_HOOKS_DIR="$(rubble-exec dirname -- "${rubble_hooks[pre-publish]}")"
+
+rubble_hook_present() {
+  [[ -f "${rubble_hooks[$1]}" && -s "${rubble_hooks[$1]}" ]]
+}
+
+rubble_run_hook() {
+  local name="$1"
+  if rubble_hook_present "${name}"; then
+    printf '[remote] running %s hook\n' "${name}" >&2
+    (builtin cd -- "${RUBBLE_REPOSITORY}" && "${rubble}" script --inherit-env "${rubble_hooks[${name}]}")
+  fi
+}
+
+# Only runner invocations use runner paths. Local hooks retain local tool selection.
+declare -A rubble_runner_commands=()
+if [[ "${RUBBLE_ON_RUNNER:-}" == 1 ]]; then
+  case "${RUBBLE_BUILD_PLATFORM}" in
+
+    'x86_64-linux')
+
+      ;;
+
+    *) printf '[remote] unknown runner platform: %s\n' "${RUBBLE_BUILD_PLATFORM}" >&2; return 1 ;;
+  esac
+fi
+
+# Extra commands are available to hooks that source this library.
+rubble-command() {
+  local name="$1"
+  shift
+  if [[ -n "${rubble_runner_commands[${name}]:-}" ]]; then
+    local executable="${rubble_runner_commands[${name}]}"
+    [[ -f "${executable}" && -x "${executable}" ]] || {
+      printf '[remote] declared runner command %s is unavailable: %s\n' "${name}" "${executable}" >&2
+      return 1
+    }
+    "${executable}" "$@"
+  elif [[ "${name}" == gh ]]; then
+    command gh "$@"
+  else
+    rubble-exec "${name}" "$@"
+  fi
+}
+
